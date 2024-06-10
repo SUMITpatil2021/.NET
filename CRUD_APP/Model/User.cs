a1d44d393a56491205bebd8d0f2569ac128ce902
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-06-11 00:47:14 +0530

    simple CRUD operation

---
+++ CRUD_APP/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD_APP.Model
+{
+	public class User
+	{
+        public int id { get; set; }
+		public string name { get; set; }
+		public int age { get; set; }
+
+		public string qualification { get; set; }
+		
+    }
+}
